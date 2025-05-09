fe6abac8968da70cfe31ae814f1a475ec947c370
michmazbout <eliasalalam@outlook.com>
2025-05-09 23:50:30 +0200

    contact me fix mobile

--- src/components/contactMeSection/ContactMeMain.jsx
+++ src/components/contactMeSection/ContactMeMain.jsx
@@ -8,7 +8,7 @@ const ContactMeMain = () => {
       className="max-w-[1200px] mx-auto items-center justify-center mt-[100px] px-4 "
     >
       <h2 className="text-6xl text-glowingPink mb-10 text-center">Contact Me</h2>
-      <div className="flex justify-between gap-24 bg-darkBackground p-8 rounded-2xl lg:flex-row sm:flex-col">
+      <div className="flex flex-col justify-between gap-24 bg-darkBackground p-8 rounded-2xl lg:flex-row sm:flex-col">
         <ContactMeLeft />
         <ContactMeRight />
       </div>
--- src/components/contactMeSection/SingleInfo.jsx
+++ src/components/contactMeSection/SingleInfo.jsx
@@ -1,6 +1,6 @@
 const SingleInfo = ({ text, Image }) => {
   return (
-    <div className="flex gap-4 items-center justify-start">
+    <div className="flex flex-col gap-4 items-center justify-start">
       <Image className="text-3xl" />
       <p>{text}</p>
     </div>
